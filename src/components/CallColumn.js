4aad88c2cead6e183574793034d11f0149518cf7
Mario Mitic <mariomitic@yahoo.com>
2023-06-11 12:31:20 -0700

    start

--- src/components/CallColumn.js
+++ src/components/CallColumn.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './MainStyle.css'
 
-<<<<<<< HEAD
 function CallColumn() {
   return (
     
@@ -28,43 +27,5 @@ function CallColumn() {
     
   )
 }
-=======
-function CallColumn(props) {
-
-  
-    return (
-
-
-      
-          <div className='callColumn'>
-      <div className='cellBorder'><b>Call</b></div>
-      <div id='callone' column='call' onClick={props.calcOnes} className={props.calledCellOne ? 'calledCell clickable cellBorder' : (props.temporaryMutedCell ? 'temporarilyMuted cellBorder' : (!props.calloneMute ? 'clickable cellBorder' : 'clickable cellBorder muted'))}><b>{props.callone}</b></div>
-      <div id='calltwo' column='call' onClick={props.calcTwos} className={props.calledCellTwo ? 'calledCell clickable cellBorder' : (props.temporaryMutedCell ? 'temporarilyMuted cellBorder' : (!props.calltwoMute ? 'clickable cellBorder' : 'clickable cellBorder muted'))}><b>{props.calltwo}</b></div>
-      <div id='callthree' column='call' onClick={props.calcThrees} className={props.calledCellThree ? 'calledCell clickable cellBorder' : (props.temporaryMutedCell ? 'temporarilyMuted cellBorder' : (!props.callthreeMute ? 'clickable cellBorder' : 'clickable cellBorder muted'))}><b>{props.callthree}</b></div>
-      <div id='callfour' column='call' onClick={props.calcFours} className={props.calledCellFour ? 'calledCell clickable cellBorder' : (props.temporaryMutedCell ? 'temporarilyMuted cellBorder' : (!props.callfourMute ? 'clickable cellBorder' : 'clickable cellBorder muted'))}><b>{props.callfour}</b></div>
-      <div id='callfive' column='call' onClick={props.calcFives} className={props.calledCellFive ? 'calledCell clickable cellBorder' : (props.temporaryMutedCell ? 'temporarilyMuted cellBorder' : (!props.callfiveMute ? 'clickable cellBorder' : 'clickable cellBorder muted'))}><b>{props.callfive}</b></div>
-      <div id='callsix' column='call' onClick={props.calcSixes} className={props.calledCellSix ? 'calledCell clickable cellBorder' : (props.temporaryMutedCell ? 'temporarilyMuted cellBorder' : (!props.callsixMute ? 'clickable cellBorder' : 'clickable cellBorder muted'))}><b>{props.callsix}</b></div>
-
-      <div className='sumBackground cellBorder'><b>{props.callsum1_6}</b></div>
-
-      <div id='callmax' column='call' onClick={props.calcMax} className={props.calledCellMax ? 'calledCell clickable cellBorder' : (props.temporaryMutedCell ? 'temporarilyMuted cellBorder' : (!props.callmaxMute ? 'clickable cellBorder' : 'clickable cellBorder muted'))}><b>{props.callmax}</b></div>
-      <div id='callmin' column='call' onClick={props.calcMin} className={props.calledCellMin ? 'calledCell clickable cellBorder' : (props.temporaryMutedCell ? 'temporarilyMuted cellBorder' : (!props.callminMute ? 'clickable cellBorder' : 'clickable cellBorder muted'))}><b>{props.callmin}</b></div>
-
-      <div className='sumBackground cellBorder'><b>{props.callsumax_min}</b></div>
-
-      <div id='callthreeofakind' column='call' onClick={props.calcThreeOfaKind} className={props.calledCellthreeOfaKind ? 'calledCell clickable cellBorder' : (props.temporaryMutedCell ? 'temporarilyMuted cellBorder' : (!props.callthreeOfaKindMute ? 'clickable cellBorder' : 'clickable cellBorder muted'))}><b>{props.callthreeOfaKind}</b></div>
-      <div id='callscale' column='call' onClick={props.calcScale} className={props.calledCellScale ? 'calledCell clickable cellBorder' : (props.temporaryMutedCell ? 'temporarilyMuted cellBorder' : (!props.callscaleMute ? 'clickable cellBorder' : 'clickable cellBorder muted'))}><b>{props.callscale}</b></div>
-      <div id='callfull' column='call' onClick={props.calcFull} className={props.calledCellFull ? 'calledCell clickable cellBorder' : (props.temporaryMutedCell ? 'temporarilyMuted cellBorder' : (!props.callfullMute ? 'clickable cellBorder' : 'clickable cellBorder muted'))}><b>{props.callfull}</b></div>
-      <div id='callpoker' column='call' onClick={props.calcPoker} className={props.calledCellPoker ? 'calledCell clickable cellBorder' : (props.temporaryMutedCell ? 'temporarilyMuted cellBorder' : (!props.callpokerMute ? 'clickable cellBorder' : 'clickable cellBorder muted'))}><b>{props.callpoker}</b></div>
-      <div id='callyamb' column='call' onClick={props.calcYamb} className={props.calledCellYamb ? 'calledCell clickable cellBorder' : (props.temporaryMutedCell ? 'temporarilyMuted cellBorder' : (!props.callyambMute ? 'clickable cellBorder' : 'clickable cellBorder muted'))}><b>{props.callyamb}</b></div>
-
-      <div className='sumBackground cellBorder'><b>{props.callsumbottom}</b></div>
-          </div>
-  
-     
-    )
-  }
-  
->>>>>>> 357457e3f5a3f7d7c0287d7946c2b6f6b60329e8
 
 export default CallColumn
--- src/components/SumColumn.js
+++ src/components/SumColumn.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import './MainStyle.css'
 
-<<<<<<< HEAD
-function DownColumn() {
+function SumColumn() {
 
 
-=======
-function SumColumn(props) {
->>>>>>> 357457e3f5a3f7d7c0287d7946c2b6f6b60329e8
 
   return (
     <div>
@@ -19,17 +15,17 @@ function SumColumn(props) {
         <div className='sumSpacing cellBorder'></div>
         <div className='sumSpacing cellBorder'></div>
         <div className='sumSpacing cellBorder'></div>
-     <div className='sumBackground cellBorder'><b>{props.sum1_6Display}</b></div>
+     <div className='sumBackground cellBorder'><b></b></div>
         <div className='sumSpacing cellBorder'></div>
         <div className='sumSpacing cellBorder'></div>
-     <div className=' sumBackground cellBorder'><b>{props.sumMax_MinDisplay}</b></div>
+     <div className=' sumBackground cellBorder'><b></b></div>
         <div className='sumSpacing cellBorder'></div>
         <div className='sumSpacing cellBorder'></div>
         <div className='sumSpacing cellBorder'></div>
         <div className='sumSpacing cellBorder'></div>
         <div className='sumSpacing cellBorder'></div>
-     <div className='sumBackground cellBorder'><b>{props.sumBottomDisplay}</b></div>
-     <div className='sumBackground cellBorder'><b>{props.sumSumDisplay}</b></div>
+     <div className='sumBackground cellBorder'><b></b></div>
+     <div className='sumBackground cellBorder'><b></b></div>
      </div>
      
 
